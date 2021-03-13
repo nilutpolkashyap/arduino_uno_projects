73f898750f8d6517e38f319a13f3504fe0dd111b
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-03-14 03:44:40 +0530

    Update motor_test_code.ino

--- motor_code/motor_test_code.ino
+++ motor_code/motor_test_code.ino
@@ -1,1 +1,21 @@
 
+void setup()
+{
+  for(int i=4;i<6;i++)
+  {
+    pinMode(i,OUTPUT);
+  }
+}
+
+void loop()
+{
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+
+  delay(5000);
+
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+
+  delay(5000);
+}
