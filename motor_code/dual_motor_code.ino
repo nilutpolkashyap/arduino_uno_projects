ef935e01ac6d08ad4a96983c97409a3f59757256
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-03-14 03:44:11 +0530

    Update dual_motor_code.ino

--- motor_code/dual_motor_code.ino
+++ motor_code/dual_motor_code.ino
@@ -1,1 +1,25 @@
 
+void setup()
+{
+  for(int i=4;i<8;i++)
+  {
+    pinMode(i,OUTPUT);
+  }
+}
+
+void loop()
+{
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+
+  delay(5000);
+
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+
+  delay(5000);
+}
