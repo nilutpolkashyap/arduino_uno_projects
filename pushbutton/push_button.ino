9ac4846e32cc1a1eca1b8692eb4831fdb65ec3ea
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-05-14 14:41:49 +0530

    Update push_button.ino

--- pushbutton/push_button.ino
+++ pushbutton/push_button.ino
@@ -1,1 +1,15 @@
 
+int pin1state = 0;
+
+void setup() {
+  pinMode(3, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  pin1state = digitalRead(3);
+
+  Serial.println("Pin 1 state : ");
+  Serial.println(pin1state);
+  Serial.print("\n");
+}
